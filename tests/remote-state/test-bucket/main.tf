6cc4a0e9a91986d9f09f46b66ac58f37adf5bedf
Daniel Flook <daniel@flook.org>
2020-07-12 21:01:58 +0100

    :sparkles: Add outputs for complex types
    Outputs of complex terraform types are now available as action outputs,
    cast to JSON types

--- tests/remote-state/test-bucket/main.tf
+++ tests/remote-state/test-bucket/main.tf
@@ -32,3 +32,84 @@ output "my_sensitive_bool" {
   value = false
   sensitive = true
 }
+
+output "my_list" {
+  value = tolist(toset(["one", "two"]))
+}
+
+output "my_sensitive_list" {
+  value = tolist(toset(["one", "two"]))
+  sensitive = true
+}
+
+output "my_map" {
+  value = tomap({
+    first = "one"
+    second = "two"
+    third = 3
+  })
+}
+
+output "my_sensitive_map" {
+  value = tomap({
+    first = "one"
+    second = "two"
+    third = 3
+  })
+  sensitive = true
+}
+
+output "my_set" {
+  value = toset(["one", "two"])
+}
+
+output "my_sensitive_set" {
+  value = toset(["one", "two"])
+  sensitive = true
+}
+
+output "my_object" {
+  value = {
+    first = "one"
+    second = "two"
+    third = 3
+  }
+}
+
+output "my_sensitive_object" {
+  value = {
+    first = "one"
+    second = "two"
+    third = 3
+  }
+  sensitive = true
+}
+
+output "my_tuple" {
+  value = ["one", "two"]
+}
+
+output "my_sensitive_tuple" {
+  value = ["one", "two"]
+  sensitive = true
+}
+
+output "my_compound_output" {
+  value = {
+    first = tolist(toset(["one", "two"]))
+    second = toset(["one", "two"])
+    third = 3
+  }
+}
+
+output "awkward_string" {
+  value = "hello \"there\", here are some 'quotes'."
+}
+
+output "awkward_compound_output" {
+  value = {
+    nested = {
+      thevalue = ["hello \"there\", here are some 'quotes'."]
+    }
+  }
+}
