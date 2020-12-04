2a8f30786ab31fd36583419c72fcf79e3f17dfca
Daniel Flook <daniel@flook.org>
2020-12-04 22:13:54 +0000

    Fix tests

--- image/tools/compact_plan.py
+++ image/tools/compact_plan.py
@@ -6,10 +6,6 @@ if __name__ == '__main__':
 
     for line in sys.stdin.readlines():
 
-        if not plan and line.startswith('---'):
-            plan = True
-            continue
-
         if not plan and (
             line.startswith('An execution plan has been generated and is shown below') or
             line.startswith('No changes') or
@@ -18,10 +14,10 @@ if __name__ == '__main__':
             plan = True
 
         if plan:
-            print(line)
+            sys.stdout.write(line)
         else:
             buffer.append(line)
 
     if not plan and buffer:
         for line in buffer:
-            print(line)
+            sys.stdout.write(line)
