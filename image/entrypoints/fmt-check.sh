41e9dbab26e1647c5a78063786f949bde34e9a80
Daniel Flook <daniel@flook.org>
2021-06-06 10:35:15 +0100

    Remove redundant exit code stuff

--- image/entrypoints/fmt-check.sh
+++ image/entrypoints/fmt-check.sh
@@ -5,18 +5,14 @@ source /usr/local/actions.sh
 debug
 setup
 
-EXIT_CODE=0
 terraform fmt -recursive -no-color -check -diff "$INPUT_PATH" | while IFS= read -r line; do
     echo "$line"
 
     if [[ -f "$line" ]]; then
         echo "::error file=$line::File is not in canonical format (terraform fmt)"
-        EXIT_CODE=1
     fi
 done
 
-if [[ "$EXIT_CODE" -eq 0 ]]; then
-    echo "All terraform configuration files are formatted correctly."
-fi
+# terraform fmt has non zero exit code if there are non canonical files
 
-exit $EXIT_CODE
+echo "All terraform configuration files are formatted correctly."
