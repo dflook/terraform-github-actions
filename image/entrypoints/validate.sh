99535aeeeb39d43d12f4f33ff2091768e79d2630
Daniel Flook <daniel@flook.org>
2022-02-15 15:34:06 +0000

    Use correct value for terraform.workspace when validating with remote backend
    Now we know the backend type, we can use the correct value for validation with
    remote backends

--- image/entrypoints/validate.sh
+++ image/entrypoints/validate.sh
@@ -11,12 +11,18 @@ setup
 # How do you get a full validation report? You can't.
 
 # terraform.workspace will be evaluated during a validate, but it is not initialized properly.
-# Pass through the workspace input, even if it doesn't make sense for some backends.
+# Pass through the workspace input, except for remote backend where it should be 'default'
+
+if [[ "$TERRAFORM_BACKEND_TYPE" == "remote" ]]; then
+  TF_WORKSPACE="default"
+else
+  TF_WORKSPACE="$INPUT_WORKSPACE"
+fi
 
 init || true
 
-if ! (cd "$INPUT_PATH" && TF_WORKSPACE="$INPUT_WORKSPACE" terraform validate -json | convert_validate_report "$INPUT_PATH"); then
-    (cd "$INPUT_PATH" && TF_WORKSPACE="$INPUT_WORKSPACE" terraform validate)
+if ! (cd "$INPUT_PATH" && TF_WORKSPACE="$TF_WORKSPACE" terraform validate -json | convert_validate_report "$INPUT_PATH"); then
+    (cd "$INPUT_PATH" && TF_WORKSPACE="$TF_WORKSPACE" terraform validate)
 else
     echo -e "\033[1;32mSuccess!\033[0m The configuration is valid"
 fi
