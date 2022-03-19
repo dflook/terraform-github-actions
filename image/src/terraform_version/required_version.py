15c3a24641d9be020ce742eb2e424bd21e9ddead
Daniel Flook <daniel@flook.org>
2022-03-19 15:24:16 +0000

    Additional logging

--- image/src/terraform_version/required_version.py
+++ image/src/terraform_version/required_version.py
@@ -8,10 +8,14 @@ from terraform.versions import Version, apply_constraints, latest_version
 def get_required_version(module: TerraformModule, versions: Iterable[Version]) -> Optional[Version]:
     constraints = get_version_constraints(module)
     if constraints is None:
+        debug('No required_version constraint found')
         return None
+
+    debug(f'Trying to match constraint {constraints}')
 
     valid_versions = list(apply_constraints(versions, constraints))
     if not valid_versions:
+        debug('No valid versions of terraform match the constraint')
         raise RuntimeError(f'No versions of terraform match the required_version constraints {constraints}\n')
 
     return latest_version(valid_versions)
