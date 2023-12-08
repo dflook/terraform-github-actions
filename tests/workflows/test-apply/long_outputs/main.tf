bc76e47c861ad538361116569b2fce12919a6f51
Daniel Flook <daniel@flook.org>
2023-12-08 10:19:18 +0000

    Fix long outputs test

--- tests/workflows/test-apply/long_outputs/main.tf
+++ tests/workflows/test-apply/long_outputs/main.tf
@@ -1,8 +1,17 @@
 variable "input" {
     type = string
-    default = "This is my long string"
+    default = "This is my long string."
+}
+
+resource "random_string" "s" {
+  length = 200
+  special = false
 }
 
 output "output" {
-  value = join('\n', [for i in range(0, 5000): var.input])
+  value = join("\n", [for i in range(0, 100): var.input])
+}
+
+output "unknown" {
+  value = join("\n", [for i in range(0, 1024): random_string.s.result])
 }
