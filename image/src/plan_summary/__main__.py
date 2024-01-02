e7e3cc6a7f786e2c0ac75f8b63fbbe16cd49b63d
Daniel Flook <daniel@flook.org>
2024-01-02 17:26:03 +0000

    Fix the plan summary outputs for new operations.
    Generalise to work with new operations such as import and the upcoming remove

--- image/src/plan_summary/__main__.py
+++ image/src/plan_summary/__main__.py
@@ -16,16 +16,42 @@ import re
 import sys
 from github_actions.commands import output
 
+def summary(plan: str) -> dict[str, int]:
+    operations = {
+        'add': 0,
+        'change': 0,
+        'destroy': 0,
+        'move': None,
+        'import': 0
+    }
+
+    to_move = 0
+
+    for line in plan.splitlines():
+        if re.match(r'  # \S+ has moved to \S+$', line):
+            to_move += 1
+
+        if not line.startswith('Plan:'):
+            continue
+
+        for op in re.finditer(r'(\d+) to (\w+)', line):
+            operations[op[2]] = int(op[1])
+
+    if operations['move'] is None:
+        operations['move'] = to_move
+
+    return operations
+
+
 def main() -> None:
-    """Entrypoint for terraform-backend"""
+    """Entrypoint for plan_summary"""
 
     with open(sys.argv[1]) as f:
         plan = f.read()
 
-    if match := re.search(r'^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy', plan, re.MULTILINE):
-        output('to_add', match[1])
-        output('to_change', match[2])
-        output('to_destroy', match[3])
+    for action, count in summary(plan).items():
+        output(f'to_{action}', count)
+
 
 if __name__ == '__main__':
     sys.exit(main())
