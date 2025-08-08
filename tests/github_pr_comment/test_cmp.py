b723c6a50b5f561b3990f62aab76f185a3ec4835
Daniel Flook <daniel@flook.org>
2025-08-08 11:44:26 +0100

    Better detect the plan summary line
    This is so we can ignore any warning text that follows.
    If a plan contains import operations, the line is different.

--- tests/github_pr_comment/test_cmp.py
+++ tests/github_pr_comment/test_cmp.py
@@ -21,6 +21,80 @@ def test_remove_unchanged_attributes():
 
     assert remove_unchanged_attributes(plan) == expected
 
+
+def test_remove_warnings_with_import():
+    plan = '''
+Terraform used the selected providers to generate the following execution
+plan. Resource actions are indicated with the following symbols:
+  + create
+
+Terraform will perform the following actions:
+
+  # random_string.count[0] will be created
+  + resource "random_string" "count" {
+      + id          = (known after apply)
+      + length      = 5
+      + lower       = true
+      + min_lower   = 0
+      + min_numeric = 0
+      + min_special = 0
+      + min_upper   = 0
+      + number      = true
+      + numeric     = true
+      + result      = (known after apply)
+      + special     = false
+      + upper       = true
+    }
+
+Plan: 26 to import, 1 to add, 0 to change, 0 to destroy.
+
+Changes to Outputs:
+  + count = (known after apply)
+
+Warning: Resource targeting is in effect
+
+You are creating a plan with the -target option, which means that the result
+of this plan may not represent all of the changes requested by the current
+configuration.
+
+The -target option is not for routine use, and is provided only for
+exceptional situations such as recovering from errors or mistakes, or when
+Terraform specifically suggests to use it as part of an error message.  
+    '''
+
+    expected = '''
+Terraform used the selected providers to generate the following execution
+plan. Resource actions are indicated with the following symbols:
+  + create
+
+Terraform will perform the following actions:
+
+  # random_string.count[0] will be created
+  + resource "random_string" "count" {
+      + id          = (known after apply)
+      + length      = 5
+      + lower       = true
+      + min_lower   = 0
+      + min_numeric = 0
+      + min_special = 0
+      + min_upper   = 0
+      + number      = true
+      + numeric     = true
+      + result      = (known after apply)
+      + special     = false
+      + upper       = true
+    }
+
+Plan: 26 to import, 1 to add, 0 to change, 0 to destroy.
+
+Changes to Outputs:
+  + count = (known after apply)
+
+    '''
+
+    assert remove_warnings(plan).strip() == expected.strip()
+
+
 def test_remove_warnings():
     plan = '''
 Terraform used the selected providers to generate the following execution
